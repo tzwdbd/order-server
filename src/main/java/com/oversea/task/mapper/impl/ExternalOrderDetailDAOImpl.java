eec78126dcb01733d08c9f971058f3ce4c278322
liuxf <liuxf@taofen8.com>
2017-09-27 10:02:46 +0800

    增加命名空间

--- src/main/java/com/oversea/task/mapper/impl/ExternalOrderDetailDAOImpl.java
+++ src/main/java/com/oversea/task/mapper/impl/ExternalOrderDetailDAOImpl.java
@@ -11,46 +11,47 @@ import com.oversea.task.mapper.BaseDao;
 import com.oversea.task.mapper.ExternalOrderDetailDAO;
 @Repository
 public class ExternalOrderDetailDAOImpl extends BaseDao implements ExternalOrderDetailDAO {
+	private static final String NAMESPACE = "ExternalOrderDetailSQL.";
 	public void addExternalOrderDetail(ExternalOrderDetail externalOrderDetail) {
-		getSqlSession().insert("addExternalOrderDetail",externalOrderDetail);
+		getSqlSession().insert(NAMESPACE+"addExternalOrderDetail",externalOrderDetail);
 }
 	public void updateExternalOrderDetailById(ExternalOrderDetail externalOrderDetail) {
-		getSqlSession().update("updateExternalOrderDetailById",externalOrderDetail);
+		getSqlSession().update(NAMESPACE+"updateExternalOrderDetailById",externalOrderDetail);
 }
 	public void updateExternalOrderDetailByDynamic(ExternalOrderDetail externalOrderDetail) {
-		getSqlSession().update("updateExternalOrderDetailByDynamic",externalOrderDetail);
+		getSqlSession().update(NAMESPACE+"updateExternalOrderDetailByDynamic",externalOrderDetail);
 }
 	public ExternalOrderDetail getExternalOrderDetailById(Long id) {
-		return getSqlSession().selectOne("getExternalOrderDetailById", id);
+		return getSqlSession().selectOne(NAMESPACE+"getExternalOrderDetailById", id);
 }
 	public int countExternalOrderDetailById(Long id) {
-		return (Integer)getSqlSession().selectOne("countExternalOrderDetailById",id);
+		return (Integer)getSqlSession().selectOne(NAMESPACE+"countExternalOrderDetailById",id);
 }
 	@Override
 	public List<ExternalOrderDetail> getExternalOrderDetailByStatus(int status) {
 		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("status", status);
-		return getSqlSession().selectList("getExternalOrderDetailByStatus", map);
+		return getSqlSession().selectList(NAMESPACE+"getExternalOrderDetailByStatus", map);
 	}
 	@Override
 	public void updateStatus(int status,Long id) {
 		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("status", status);
 		map.put("id", id);
-		getSqlSession().update("updateStatus", map);
+		getSqlSession().update(NAMESPACE+"updateStatus", map);
 	}
 	@Override
 	public List<ExternalOrderDetail> getExternalOrderDetailByOrderNo(
 			String saleOrderCode) {
 		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("saleOrderCode", saleOrderCode);
-		return getSqlSession().selectList("getExternalOrderDetailByOrderNo", map);
+		return getSqlSession().selectList(NAMESPACE+"getExternalOrderDetailByOrderNo", map);
 	}
 	@Override
 	public List<ExternalOrderDetail> findExternalOrderDetailsForSpiderExpress(
 			String status) {
 		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("status", status);
-		return getSqlSession().selectList("findExternalOrderDetailsForSpiderExpress", map);
+		return getSqlSession().selectList(NAMESPACE+"findExternalOrderDetailsForSpiderExpress", map);
 	}
 }
